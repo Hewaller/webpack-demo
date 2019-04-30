3cbb547b42b4ef6fc75872cb386e6f0c7f1cb215
henwq <wenquan_he@163.com>
2019-04-30 11:59:40 +0800

    手动搭建一个Vue项目

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 // src/App.vue
 <template>
-  <div class="App">Hello World</div>
+  <div class="App">
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
 <script>
@@ -15,6 +21,11 @@ export default {
 
 <style lang="scss" scoped>
 .App {
-  color: blue;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
